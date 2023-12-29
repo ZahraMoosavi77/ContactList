0c1e279b33202337a411affcd260b34ad351a023
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-12-29 19:04:54 +0330

    fix: fixed name of project

--- src/components/modules/Pagination/Pagination.jsx
+++ src/components/modules/Pagination/Pagination.jsx
@@ -34,13 +34,17 @@ const Pagination = ({ pagesCount, currentPage, query }) => {
       <div className="inline-flex mt-2 xs:mt-0">
         <button
           onClick={prevPageHandler}
-          className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 rounded-s hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 rounded-s
+           hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700
+            dark:hover:text-white"
         >
           Prev
         </button>
         <button
           onClick={nextPageHandler}
-          className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 border-0 border-s border-gray-700 rounded-e hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white
+           bg-gray-800 border-0 border-s border-gray-700 rounded-e hover:bg-gray-900 dark:bg-gray-800
+            dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
         >
           Next
         </button>
